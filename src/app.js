4ca197eb6307955d740892bc32f494541e56ac0b
Navarroseb <sebastnavarro@gmail.com>
2022-01-28 20:35:40 +0000

    Card Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomsymbol());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+  setTimeout(() => {
+    document.querySelector(".card").classList.remove("heart");
+    document.querySelector(".card").classList.add("spade");
+  }, 5000);
 };
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexnumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexnumbers];
+};
+
+let generateRandomsymbol = () => {
+  let symbols = ["diamond", "club", "heart", "spade"];
+
+  let indexsymbols = Math.floor(Math.random() * symbols.length);
+  return symbols[indexsymbols];
+};
+
+const boton = document.createElement("button");
+boton.type = "button";
+boton.innerText = "Haz Click";
+document.body.appendChild(boton);
